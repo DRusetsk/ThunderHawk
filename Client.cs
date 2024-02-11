8ab4fe5e56125e8f919b9539e6a4d838dc3bcbee
Daniel <dukarus007@gmail.com>
2024-02-10 20:00:55 -0500

    Code fully functioning

---
+++ Client.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RDR2;
+using RDR2.Math;
+using RDR2.Native;
+using RDR2.UI;
+
+namespace ThunderHawk
+{
+    public class Client : Script
+    {
+        private Vector3 lastHitPos = Vector3.Zero;
+        public Client() {
+            Tick +=OnTick;
+            Interval = 1;
+        }
+
+        private void OnTick(object sender, EventArgs e)
+        {
+            var out1 = new OutputArgument();
+            Function.Call(Hash.GET_PED_LAST_WEAPON_IMPACT_COORD, Game.Player.Character.Handle, out1);
+            var pos = out1.GetResult<Vector3>();
+            if (pos.DistanceTo(Game.Player.Character.Position) < 200f && lastHitPos!=pos)
+            {
+                Function.Call((Hash)0x67943537D179597C, pos.X,pos.Y, pos.Z);
+                lastHitPos = pos;
+            }
+        }
+    }
+}
